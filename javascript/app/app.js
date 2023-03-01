a75e823cf9d700cdc368cd6b26a639a61cfa5c8b
Domains18 <gibsonsgibson88@gmail.com>
2023-03-01 23:11:08 +0300

    added app

---
+++ javascript/app/app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const path = require('path');
+const colors = require('colors');
+const cors = require('cors');
+
