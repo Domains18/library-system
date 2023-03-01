84a2d357498f4a32a0b84a7c33d86ab9451e75e9
Domains18 <gibsonsgibson88@gmail.com>
2023-03-01 23:21:29 +0300

    added app

--- javascript/app/app.js
+++ javascript/app/app.js
@@ -1,9 +1,24 @@
 const express = require('express');
-const dotenv = require('dotenv');
+const dotenv = require('dotenv').config();
 const bodyParser = require('body-parser');
 const path = require('path');
 const colors = require('colors');
 const cors = require('cors');
-
+const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Routes
+app.use('/', async (req, res) => {
+    res.send('Hello World');
+});
+
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`.yellow.bold));
